735b452576eea7ade3801532045cc3f46594374f
Emircan EGE <ege61emir@gmail.com>
2024-11-21 21:41:56 +0300

    Add CategoryController with CRUD endpoints

---
+++ ECommerce.Api/Controllers/CategoryController.cs
@@ -0,0 +1,75 @@
+﻿using ECommerce.Business.Services.Interfaces;
+using ECommerce.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var category = await _categoryService.GetByIdAsync(id);
+                return Ok(category);
+            }
+            catch (Exception ex) 
+            { 
+                return NotFound(new { message = ex.Message});
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateCategoryDto createCategoryDto)
+        {
+            await _categoryService.AddAsync(createCategoryDto);
+            return Created("", new {message = "Category created successfully"});
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] UpdateCategoryDto updateCategoryDto)
+        {
+            try
+            {
+                await _categoryService.UpdateAsync(id, updateCategoryDto);
+                return Ok(new { message = "Category updated successfully" });
+            }
+            catch (Exception ex)
+            {
+                return NotFound(new { message = ex.Message});
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _categoryService.DeleteAsync(id);
+                return Ok(new { message = "Category deleted  successfully"});
+            }
+            catch(Exception ex)
+            {
+                return NotFound(new { message = ex.Message});
+            }
+
+        }
+    }
+}
